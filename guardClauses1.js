a5bc37053b1b2071213acab65459cccb9eabdbbd
DesmondM <desmondmpofu@gmail.com>
2021-03-24 08:52:04 +0200

    amended guardCluases function

--- guardClauses1.js
+++ guardClauses1.js
@@ -1,6 +1,6 @@
 
 function canDrink(person){
-    if(person?.age==null){
+    if(person?.age!=null){
         if(person.age<18){
             console.log("Noy you cannot")
         } else if (person.age<21){
@@ -23,3 +23,25 @@ const c ={
 
 canDrink(p)
 canDrink(c)
+
+
+function canDrinkBetter(person){
+    if(person?.age==null){
+        console.log("You are not a person")
+        return
+    }
+
+    const result=canDrinkResponse(person.age)
+    console.log(result)
+}
+
+function canDrinkResponse(age){
+    if(age<18) return "Fxn 2: No you cannot drink"
+    if (age<21) return "Fxn 2:You cannot drink in the US"
+    return "Fxn 2:Yes"
+}
+
+const r ={
+    age: 45
+}
+canDrinkBetter(r)

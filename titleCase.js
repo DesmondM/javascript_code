7d56811378b899ab5fde88df0487f1bfbb1a206e
DesmondM <desmondmpofu@gmail.com>
2021-06-14 13:52:55 +0200

    title case

---
+++ titleCase.js
@@ -0,0 +1,9 @@
+const titleCase = (str) =>{
+    var str = str.toLowerCase().split(" ");
+    for(let i =0; i< str.length; i++){
+        str[i] = str[i][0].toUpperCase()+ str[i].slice(1);
+    }
+    return str.join(" ")
+}
+
+console.log(titleCase("It is the day that the lord has made, we will rejoice and be glad in it"));

6d615927d8823e27666d80833539d41c280e0967
DesmondM <desmondmpofu@gmail.com>
2021-07-23 23:54:22 +0200

    practice

---
+++ practice.js
@@ -0,0 +1,19 @@
+const todos = [
+    {
+        id: 1,
+        text: 'Take out trash',
+        isCompleted: true
+    },
+    {
+        id: 2,
+        text: 'Code Java',
+        isCompleted: true
+    },
+    {
+        id: 1,
+        text: 'Eat Lunch',
+        isCompleted: false
+    }
+];
+
+console.log(todos);

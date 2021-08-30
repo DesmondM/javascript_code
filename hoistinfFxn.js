6d968acb34265ac486d38bdd3a8ba0db685e214c
DesmondM <desmondmpofu@gmail.com>
2021-08-30 21:09:34 +0200

    hoisting

---
+++ hoistinfFxn.js
@@ -0,0 +1,3 @@
+console.log(hoistedVariable); 
+var hoistedVariable;
+hoistedVariable = 3;
---
+++ symbolFxn.js
@@ -0,0 +1,6 @@
+const symb = ()=>{
+    var symbol1 = Symbol('symbol');
+    console.log(symbol1)
+}
+
+symb();

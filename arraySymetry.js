1a4bdb6f8ef64c0da82fdc3cbe4224e993a9f7c6
DesmondM <desmondmpofu@gmail.com>
2021-10-06 22:32:10 +0200

    symmetry

--- arraySymetry.js
+++ arraySymetry.js
@@ -1,18 +1,14 @@
-const arrayDiff =(arr1, arr2) =>{
-
-    var result = [];
-
-    for(var i =0; i<arr1.length; i++){
-        if(arr2.indexOf(arr1[i])===-1){
-            result.push(arr1[i])
-        }
+const symmetry =(arr1, arr2)=>{
+    var newArr = [];
+    for(var i=0; i<arr1.length; i++){
+        if(arr2.indexOf(arr1[i])===-1)
+        newArr.push(arr1[i])
     }
-    for(var j=0; j<arr2.length; j++){
-        if(arr1.indexOf(arr2[j])===-1){
-            result.push(arr2[j])
-        }
+
+    for (var j=0; j<arr2.length; j++){
+        if(arr1.indexOf(arr2[j])===-1)
+        newArr.push(arr2[j])
     }
-    return result;
+    return newArr
 }
-
-console.log(arrayDiff([2,4,6,8,9,10,11,12,13], [1,3,5,7,9,10,11,12,13] ));
+console.log(symmetry([1,2,3,4,5,6, 7], [1,2,3,10,12, 14, 16]))

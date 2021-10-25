b86a67f66aa59707265224b1c0148355b186f0bd
DesmondM <desmondmpofu@gmail.com>
2021-10-25 20:01:29 +0200

    LCA

---
+++ lowestCATree.js
@@ -0,0 +1,17 @@
+
+const lca = (root,p,q) =>{
+    let result = null;
+
+    const dfs =(node)=>{
+        if(node===null) return false;
+        let left=dfs(node.left);
+        let right=dfs(node.right);
+        let cur= node===p||node===q;
+
+        if(left+right+cur>=2) result = node;
+        return left||right||cur;
+    }
+    
+    dfs(root);
+    return result;
+}

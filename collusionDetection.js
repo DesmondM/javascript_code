ba101f2aa26f0a842a8d1a9e7b7babf101ece074
DesmondM <desmondmpofu@gmail.com>
2021-10-31 08:07:36 +0200

    collusion detection

---
+++ collusionDetection.js
@@ -0,0 +1,12 @@
+const detect =(sqr1_x, sqr1_y,sqr1_width, sqr2_x, sqr2_y,sqr2_width)=>{
+    if(sqr1_x >sqr2_x + sqr2_width||
+      sqr1_x + sqr1_width<sqr2_x ||
+      sqr1_y>sqr2_y+sqr1_width||
+      sqr1_y+sqr1_width<sqr2_y){
+          return "There is no collusion"
+      }else{
+          return "Big bang..."
+      }
+}
+
+console.log(detect(4,6, 5, 5,8,6))
